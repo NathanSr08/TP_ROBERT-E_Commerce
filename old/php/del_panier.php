69ed104e2ca27219b215b9b0f15eac3b69f627e5
Nathan55555 <77548288+Nathan55555@users.noreply.github.com>
2022-11-24 08:17:16 +0100

    Delete old directory

--- old/php/del_panier.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('../php_obj/autoload.php');
-$p = new Panier;
-$p->del($_GET['id']);
-
-?>
-   <SCRIPT LANGUAGE="JavaScript">
-document.location.href="view_panier.php"
-</SCRIPT>
--- old/push.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "New"
-git push https://Nathan55555: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@github.com/Nathan55555/TP_ROBERT-E_Commerce.git
