5c6d20bee03a8a1f2e8caffc101a7e6668a3cee2
Nathan55555 <Nathansroussi@gmail.com>
2023-01-02 12:04:00 +0100

    New

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 git add .
 git commit -m "New"
-git push https://Nathan55555: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@github.com/Nathan55555/TP_ROBERT-E_Commerce.git
+git push https://Nathan55555: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@github.com/Nathan55555/TP_ROBERT-E_Commerce.git
